a1a17b5121e9f75ecda0aeb6fddd6b202dad6bb8
MuGdxy <lxy819469559@gmail.com>
2025-07-16 21:07:57 +0800

    fix sanity check body-self-collision-off but still invalid world problem

--- src/sanity_check/simplicial_surface_intersection_check.cpp
+++ src/sanity_check/simplicial_surface_intersection_check.cpp
@@ -232,8 +232,7 @@ class SimplicialSurfaceIntersectionCheck final : public SanityChecker
                 if(VInstanceIds[E[0]] == VInstanceIds[F[0]])
                 {
                     // if self-collision is not enabled, skip it
-                    auto Inst = VInstanceIds[E[0]];
-                    if(!SelfCollision[Inst])
+                    if(!SelfCollision[E[0]])
                         return;
                 }
 
