f33044bc3a2a9b40bd717e3b10848161df45f63e
MuGdxy <lxy819469559@gmail.com>
2025-09-19 16:08:28 +0800

    refactor

--- python/src/uipc/dev/uid_info.py
+++ python/src/uipc/dev/uid_info.py
@@ -3,37 +3,31 @@ from uipc import builtin
 # only export ConstitutionUIDInfo and ImplicitGeometryUIDInfo
 __all__ = ['ConstitutionUIDInfo', 'ImplicitGeometryUIDInfo']
 
-class UIDInfo:    
+class UIDInfo:
+    def __init__(self, uids):
+        self.uids = sorted(uids, key=lambda x: x['uid'])
+        self._uid_map = {u['uid']: u for u in self.uids}
+
     def check_uid_available(self, uid: int) -> bool:
-        uids = self.uids
-        uid_map = {u['uid']:u for u in uids}
-        if uid in uid_map.keys():
-            u = uid_map[uid]
-            name = u['name']
-            type = u['type']
-            # print with red color
-            print(f'\033[91mUID Already Exists:\n{uid} |  {name} | {type}\033[0m')
-            return False
-        return True
+        """Checks if a UID is available (i.e., not already in use)."""
+        return uid not in self._uid_map
+
+    def get_uid_info(self, uid: int):
+        """Gets information about a given UID."""
+        return self._uid_map.get(uid)
 
     def first_available_uid(self, start_uid: int=0) -> int:
-        # sort uids by uid
-        uids = self.uids
-        uid_set = {u['uid'] for u in uids}
         uid = start_uid
-        while uid in uid_set:
+        # Using self._uid_map is much faster than creating a set every time.
+        while uid in self._uid_map:
             uid += 1
         return uid
     
-    def __init__(self, uids):
-        self.uids = sorted(uids, key=lambda x: x['uid'])
-    
     def __repr__(self):
-        CONTENT = '''| UID | Name | Type |
-|-----|------|------|'''
-        for u in self.uids:
-            CONTENT += f'\n| {u["uid"]} | {u["name"]} | {u["type"]} |'
-        return CONTENT
+        header = "| UID | Name | Type |\n|-----|------|------|"
+        # Use a list comprehension and join for efficient string building.
+        rows = [f'| {u["uid"]} | {u["name"]} | {u["type"]} |' for u in self.uids]
+        return "\n".join([header] + rows)
 
 class ConstitutionUIDInfo(UIDInfo):
     def __init__(self):
