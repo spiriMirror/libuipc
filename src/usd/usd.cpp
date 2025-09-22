a4b5ddb7667d0f0b6d3bd84ee99f6a6c8767821f
MuGdxy <lxy819469559@gmail.com>
2025-09-22 11:52:50 +0800

    add usd

---
+++ src/usd/usd.cpp
@@ -0,0 +1,15 @@
+#include <pxr/usd/usd/stage.h>
+#include <pxr/usd/usdGeom/xform.h>
+#include <uipc/core/scene.h>
+
+namespace uipc::usd
+{
+void test_usd()
+{
+    auto stage = pxr::UsdStage::CreateNew("test.usda");
+    auto root  = stage->GetRootLayer();
+    // add an xform
+    auto prim  = pxr::UsdGeomXform::Define(stage, pxr::SdfPath("/Cube"));
+    auto scene = uipc::core::Scene();
+}
+}  // namespace uipc::usd
