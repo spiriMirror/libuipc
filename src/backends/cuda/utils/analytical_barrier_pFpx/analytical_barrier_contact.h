e10910bb84294bda10c39348ac92a4514f1fa22e
KemengHuang <57673908+KemengHuang@users.noreply.github.com>
2025-09-28 22:38:32 +0800

    add analytical_barrier_pFpx_PP and PE

--- src/backends/cuda/utils/analytical_barrier_pFpx/analytical_barrier_contact.h
+++ src/backends/cuda/utils/analytical_barrier_pFpx/analytical_barrier_contact.h
@@ -2,3 +2,5 @@
 #include <type_define.h>
 #include <utils/analytical_barrier_pFpx/pFpx_EE_contact.h>
 #include <utils/analytical_barrier_pFpx/pFpx_PT_contact.h>
+#include <utils/analytical_barrier_pFpx/pFpx_PE_contact.h>
+#include <utils/analytical_barrier_pFpx/pFpx_PP_contact.h>
