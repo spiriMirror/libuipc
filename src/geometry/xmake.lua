3bca6272dfe55ceaeffa90f5b4b5f455decf0fff
star9029 <hengxings783@gmail.com>
2025-10-20 15:19:12 +0800

    build(xmake): remove tetgen

--- src/geometry/xmake.lua
+++ src/geometry/xmake.lua
@@ -1,4 +1,4 @@
-add_requires("libigl", "octree", "tetgen", "tbb")
+add_requires("libigl", "octree", "tbb")
 
 target("uipc_geometry")
     add_rules("component")
@@ -11,5 +11,5 @@ target("uipc_geometry")
     )
     add_defines()
     add_deps("uipc_core")
-    add_packages("octree", "tetgen", "tbb")
+    add_packages("octree", "tbb")
     add_packages("libigl", {public = true})
