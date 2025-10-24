c6dd2088e320d99e1939732b1e7c595cc05bdbb7
MuGdxy <lxy819469559@gmail.com>
2025-10-24 21:26:07 +0800

    improve subscene test and code style

--- src/backends/cuda/contact_system/contact_models/ipc_vertex_half_plane_normal_contact.cu
+++ src/backends/cuda/contact_system/contact_models/ipc_vertex_half_plane_normal_contact.cu
@@ -36,10 +36,11 @@ class IPCVertexHalfPlaneNormalContact final : public VertexHalfPlaneNormalContac
                     plane_normals = half_plane->normals().viewer().name("plane_normals"),
                     table = info.contact_tabular().viewer().name("contact_tabular"),
                     contact_ids = info.contact_element_ids().viewer().name("contact_element_ids"),
-                    half_plane_contact_ids = half_plane->contact_ids().viewer().name("half_plane_contact_ids"),
+                    subscene_ids = info.subscene_element_ids().viewer().name("subscene_element_ids"),
                     Ps = info.positions().viewer().name("Ps"),
                     thicknesses = info.thicknesses().viewer().name("thicknesses"),
-                    eps_v  = info.eps_velocity(),
+                    eps_v                    = info.eps_velocity(),
+                    half_plane_vertex_offset = info.half_plane_vertex_offset(),
                     d_hats = info.d_hats().viewer().name("d_hats"),
                     dt     = info.dt()] __device__(int I) mutable
                    {
@@ -54,9 +55,9 @@ class IPCVertexHalfPlaneNormalContact final : public VertexHalfPlaneNormalContac
                        Vector3 P = plane_positions(HI);
                        Vector3 N = plane_normals(HI);
 
-                       Float kt2 =
-                           table(contact_ids(vI), half_plane_contact_ids(HI)).kappa
-                           * dt * dt;
+                       Float kt2 = table(contact_ids(vI), contact_ids(HI + half_plane_vertex_offset))
+                                       .kappa
+                                   * dt * dt;
 
                        Float thickness = thicknesses(vI);
 
@@ -73,50 +74,49 @@ class IPCVertexHalfPlaneNormalContact final : public VertexHalfPlaneNormalContac
         {
             ParallelFor()
                 .file_line(__FILE__, __LINE__)
-                .apply(
-                    info.PHs().size(),
-                    [Grad = info.gradients().viewer().name("Grad"),
-                     Hess = info.hessians().viewer().name("Hess"),
-                     PHs  = info.PHs().viewer().name("PHs"),
-                     plane_positions = half_plane->positions().viewer().name("plane_positions"),
-                     plane_normals = half_plane->normals().viewer().name("plane_normals"),
-                     table = info.contact_tabular().viewer().name("contact_tabular"),
-                     contact_ids = info.contact_element_ids().viewer().name("contact_element_ids"),
-                     half_plane_contact_ids = half_plane->contact_ids().viewer().name("half_plane_contact_ids"),
-                     Ps = info.positions().viewer().name("Ps"),
-                     thicknesses = info.thicknesses().viewer().name("thicknesses"),
-                     eps_v  = info.eps_velocity(),
-                     d_hats = info.d_hats().viewer().name("d_hats"),
-                     dt     = info.dt()] __device__(int I) mutable
-                    {
-                        Vector2i PH = PHs(I);
+                .apply(info.PHs().size(),
+                       [Grad = info.gradients().viewer().name("Grad"),
+                        Hess = info.hessians().viewer().name("Hess"),
+                        PHs  = info.PHs().viewer().name("PHs"),
+                        plane_positions = half_plane->positions().viewer().name("plane_positions"),
+                        plane_normals = half_plane->normals().viewer().name("plane_normals"),
+                        table = info.contact_tabular().viewer().name("contact_tabular"),
+                        contact_ids = info.contact_element_ids().viewer().name("contact_element_ids"),
+                        Ps = info.positions().viewer().name("Ps"),
+                        thicknesses = info.thicknesses().viewer().name("thicknesses"),
+                        eps_v  = info.eps_velocity(),
+                        d_hats = info.d_hats().viewer().name("d_hats"),
+                        half_plane_vertex_offset = info.half_plane_vertex_offset(),
+                        dt = info.dt()] __device__(int I) mutable
+                       {
+                           Vector2i PH = PHs(I);
 
-                        IndexT vI = PH(0);
-                        IndexT HI = PH(1);
+                           IndexT vI = PH(0);
+                           IndexT HI = PH(1);
 
-                        Vector3 v = Ps(vI);
-                        Vector3 P = plane_positions(HI);
-                        Vector3 N = plane_normals(HI);
+                           Vector3 v = Ps(vI);
+                           Vector3 P = plane_positions(HI);
+                           Vector3 N = plane_normals(HI);
 
-                        Float d_hat = d_hats(vI);
+                           Float d_hat = d_hats(vI);
 
-                        Float kt2 =
-                            table(contact_ids(vI), half_plane_contact_ids(HI)).kappa
-                            * dt * dt;
+                           Float kt2 = table(contact_ids(vI), contact_ids(HI + half_plane_vertex_offset))
+                                           .kappa
+                                       * dt * dt;
 
-                        Float thickness = thicknesses(vI);
+                           Float thickness = thicknesses(vI);
 
-                        Vector3   G;
-                        Matrix3x3 H;
+                           Vector3   G;
+                           Matrix3x3 H;
 
-                        sym::ipc_vertex_half_contact::PH_barrier_gradient_hessian(
-                            G, H, kt2, d_hat, thickness, v, P, N);
+                           sym::ipc_vertex_half_contact::PH_barrier_gradient_hessian(
+                               G, H, kt2, d_hat, thickness, v, P, N);
 
-                        cuda::make_spd(H);
+                           cuda::make_spd(H);
 
-                        Grad(I).write(PH(0), G);
-                        Hess(I).write(PH(0), PH(0), H);
-                    });
+                           Grad(I).write(vI, G);
+                           Hess(I).write(vI, vI, H);
+                       });
         }
     }
 
