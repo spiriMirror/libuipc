8dc71cdd45a55be88a0ecda3f03a9cfb502edc45
MuGdxy <lxy819469559@gmail.com>
2026-01-10 04:18:49 +0800

    add xmake docker

--- src/pybind/xmake.lua
+++ src/pybind/xmake.lua
@@ -29,9 +29,25 @@ target("pyuipc")
     end)
 
     after_build(function (target)
-        local py_module_target = "$(projectdir)" .. "/python/src/uipc/modules/" 
-        -- make module target if it doesn't exist
-        os.mkdir(py_module_target)
-        print("Copying folder from " .. target:targetdir() .. " to " .. py_module_target)
-        os.cp(target:targetdir(), py_module_target)
+        local project_dir = os.projectdir()
+        local python_source_dir = path.join(project_dir, "python")
+        local build_dir = path.join(project_dir, "build")
+        local python_build_dir = path.join(build_dir, "python")
+        local modules_target_dir = path.join(python_build_dir, "src", "uipc", "modules")
+        
+        -- Copy the entire python folder to build directory
+        print("Copying python folder from " .. python_source_dir .. " to " .. python_build_dir)
+        -- os.cp copies directories recursively when given a directory path
+        os.cp(python_source_dir, python_build_dir)
+        
+        -- Ensure modules directory exists
+        os.mkdir(modules_target_dir)
+        
+        -- Copy the built modules from target directory to build_dir/python/src/uipc/modules/
+        local target_dir = target:targetdir()
+        print("Copying modules from " .. target_dir .. " to " .. modules_target_dir)
+        
+        -- Copy all files from target directory to modules directory
+        -- Use os.cp with pattern to copy all files
+        os.cp(path.join(target_dir, "*"), modules_target_dir)
     end)
