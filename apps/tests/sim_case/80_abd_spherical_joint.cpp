29b3404672485c14b8abc307e3e43a81221328cd
ligo <1569731402@qq.com>
2026-03-23 14:24:58 +0800

    update

--- apps/tests/sim_case/80_abd_spherical_joint.cpp
+++ apps/tests/sim_case/80_abd_spherical_joint.cpp
@@ -64,7 +64,7 @@ TEST_CASE("80_abd_spherical_joint", "[abd][joint]")
     // ---- Spherical joint ----
     // Anchor is specified in body1 (right body)'s local frame.
     // The cube is scaled to 0.3, so half-extent is 0.15.
-    // r_local_pos = {-0.15, 0, 0}: the left face center of the right cube.
+    // r_local_pos = {-0.15, 0.15, -0.15}: the anchor point p of the right cube.
     SimplicialComplex                joint_mesh;
     AffineBodySphericalJoint         spherical_joint;
     vector<S<SimplicialComplexSlot>> l_geo_slots    = {left_geo_slot};
@@ -83,36 +83,6 @@ TEST_CASE("80_abd_spherical_joint", "[abd][joint]")
 
     auto joint_object = scene.objects().create("spherical_joint");
     joint_object->geometries().create(joint_mesh);
-
-    // ---- Animator: rotate + translate the left body ----
-    // Expected: right body's anchor (left face) follows the left body,
-    //           but the right body can rotate freely about the anchor.
-    // auto& animator = scene.animator();
-    // animator.insert(
-    //     *left_object,
-    //     [](Animation::UpdateInfo& info)
-    //     {
-    //         auto geo_slots = info.geo_slots();
-    //         auto geo       = geo_slots[0]->geometry().as<SimplicialComplex>();
-
-    //         auto is_constrained = geo->instances().find<IndexT>(builtin::is_constrained);
-    //         view(*is_constrained)[0] = 1;
-
-    //         auto aim = geo->instances().find<Matrix4x4>(builtin::aim_transform);
-    //         auto aim_view = view(*aim);
-
-    //         constexpr Float pi = std::numbers::pi;
-    //         Float           t  = info.frame() * info.dt();
-
-    //         Float x_offset = 0.5 * std::sin(2 * pi * t);
-    //         Float y_offset = 0.3 * std::sin(4 * pi * t);
-    //         Float theta    = pi / 2 * std::sin(2 * pi * t);
-
-    //         Transform transform = Transform::Identity();
-    //         transform.translate(Vector3{-0.5 + x_offset, 0.5 + y_offset, 0});
-    //         transform.rotate(Eigen::AngleAxisd(theta, Vector3::UnitZ()));
-    //         aim_view[0] = transform.matrix();
-    //     });
 
     world.init(scene);
     REQUIRE(world.is_valid());
